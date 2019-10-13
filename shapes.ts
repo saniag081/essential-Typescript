7b9499fbf32cf576fd07383076c1de76d6536891
Santiago <sgonzalezhernandez081@gmail.com>
2019-10-12 21:15:33 -0500

    shapes

---
+++ shapes.ts
@@ -0,0 +1,26 @@
+class Persona{
+    private edad: number;
+    private altura: number;
+    private dni: string;
+
+    constructor(edad: number,altura: number,dni: string){
+        this.edad = edad;
+        this.altura = altura;
+        this.dni = dni;
+    }
+}
+
+class Alumno extends Persona{
+    private matricula: string;
+
+    constructor(edad:number, altura: number, dni: string, matricula: string){
+        super(edad,altura,dni);
+        this.matricula = matricula;
+    }
+}
+
+let persona: Persona = new Persona(27, 1.89, '123141');
+let alumno: Alumno =new Alumno(30, 1.90, '23123','3213');
+
+persona = alumno;
+alumno = persona;
